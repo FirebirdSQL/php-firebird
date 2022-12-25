3bcbf0a004f09f2024a1e61cc09694d692bcd1b8
Jonatan Klemets <jonatan.r.klemets@gmail.com>
2022-12-25 17:36:37 +0200

    Fix ZEND_BEGIN_ARG_INFO_EX for ibase_commit
    When running a debug build of PHP, the engine verifies that the info
    supplied in the `ZEND_BEGIN_ARG_INFO_EX` macro is correct. The last
    parameter of the `ZEND_BEGIN_ARG_INFO_EX` macro is the number of
    required arguments.
    
    If we try to invoke `ibase_commit` with zero arguments, we get the
    following error message:
    
    ```
    Fatal error: Arginfo / zpp mismatch during call of ibase_commit() in test.php on line 123
    ```
    
    This patch corrects this by setting the value to zero to match how the
    method is implemented.

--- interbase.c
+++ interbase.c
@@ -91,7 +91,7 @@ ZEND_BEGIN_ARG_INFO_EX(arginfo_ibase_trans, 0, 0, 0)
 	ZEND_ARG_INFO(0, trans_args)
 ZEND_END_ARG_INFO()
 
-ZEND_BEGIN_ARG_INFO_EX(arginfo_ibase_commit, 0, 0, 1)
+ZEND_BEGIN_ARG_INFO_EX(arginfo_ibase_commit, 0, 0, 0)
 	ZEND_ARG_INFO(0, link_identifier)
 ZEND_END_ARG_INFO()
 
