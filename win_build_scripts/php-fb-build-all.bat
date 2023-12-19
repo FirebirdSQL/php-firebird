4fdfbc924100679340bba5352b232c7700a21e95
Martin Köditz <martin.koeditz@it-syn.de>
2023-12-19 09:49:59 +0100

    Added PHP 8.3 to php-fb-build-all.bat

--- win_build_scripts/php-fb-build-all.bat
+++ win_build_scripts/php-fb-build-all.bat
@@ -5,4 +5,5 @@ call php-fb-build.bat 7.4 vc15 || exit /B %ERRORLEVEL%
 call php-fb-build.bat 8.0 vs16 || exit /B %ERRORLEVEL%
 call php-fb-build.bat 8.1 vs16 || exit /B %ERRORLEVEL%
 call php-fb-build.bat 8.2 vs16 || exit /B %ERRORLEVEL%
+call php-fb-build.bat 8.3 vs16 || exit /B %ERRORLEVEL%
 call php-fb-build.bat master vs16 || exit /B %ERRORLEVEL%
