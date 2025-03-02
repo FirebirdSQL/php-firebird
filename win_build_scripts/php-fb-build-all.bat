828ba1c454898ad552a385841a7b78d8151da6e8
Martins Lazdans <marrtins@dqdp.net>
2025-03-02 04:01:21 +0200

    Add 8.4

--- win_build_scripts/php-fb-build-all.bat
+++ win_build_scripts/php-fb-build-all.bat
@@ -6,4 +6,5 @@ call php-fb-build.bat 8.0 vs16 || exit /B %ERRORLEVEL%
 call php-fb-build.bat 8.1 vs16 || exit /B %ERRORLEVEL%
 call php-fb-build.bat 8.2 vs16 || exit /B %ERRORLEVEL%
 call php-fb-build.bat 8.3 vs16 || exit /B %ERRORLEVEL%
-call php-fb-build.bat master vs16 || exit /B %ERRORLEVEL%
+call php-fb-build.bat 8.4 vs17 || exit /B %ERRORLEVEL%
+call php-fb-build.bat master vs17 || exit /B %ERRORLEVEL%
