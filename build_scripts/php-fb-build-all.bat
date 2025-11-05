103b4c691923263cfefbad420ce4883c846d1594
Mārtiņš Lazdāns <marrtins@dqdp.net>
2025-11-05 14:11:47 +0200

    Refactor out inner loops in php-fb-build.bat

--- build_scripts/php-fb-build-all.bat
+++ build_scripts/php-fb-build-all.bat
@@ -1,10 +1,16 @@
 @echo off
 
-call %~dp0php-fb-build.bat php-7.3.33 vc15 || exit %ERRORLEVEL%
-call %~dp0php-fb-build.bat php-7.4.13 vc15 || exit %ERRORLEVEL%
-call %~dp0php-fb-build.bat php-8.0.30 vs16 || exit %ERRORLEVEL%
-call %~dp0php-fb-build.bat php-8.1.33 vs16 || exit %ERRORLEVEL%
-call %~dp0php-fb-build.bat php-8.2.29 vs16 || exit %ERRORLEVEL%
-call %~dp0php-fb-build.bat php-8.3.26 vs16 || exit %ERRORLEVEL%
-call %~dp0php-fb-build.bat php-8.4.13 vs17 || exit %ERRORLEVEL%
-call %~dp0php-fb-build.bat php-8.5.0RC2 vs17 || exit %ERRORLEVEL%
+set "phps="php-7.4.13 vc15" "php-8.0.30 vs16" "php-8.1.33 vs16" "php-8.2.29 vs16" "php-8.3.26 vs16" "php-8.4.13 vs17" "php-8.5.0RC2 vs17""
+
+setlocal enabledelayedexpansion
+for %%p in (%phps%) do (
+    for /f "tokens=1,2" %%a in (%%p) do (
+        set php_vers=%%a
+        set cpp_vers=%%b
+        call %~dp0php-fb-build.bat !php_vers! !cpp_vers! 1 x64 || exit %ERRORLEVEL%
+        call %~dp0php-fb-build.bat !php_vers! !cpp_vers! 0 x64 || exit %ERRORLEVEL%
+        call %~dp0php-fb-build.bat !php_vers! !cpp_vers! 1 x86 || exit %ERRORLEVEL%
+        call %~dp0php-fb-build.bat !php_vers! !cpp_vers! 0 x86 || exit %ERRORLEVEL%
+    )
+)
+endlocal
--- build_scripts/php-fb-config.bat
+++ build_scripts/php-fb-config.bat
@@ -1,18 +1,24 @@
-@REM
-@REM git command must be in PATH
-@REM
-
+@echo off
 @REM php-firebird source directory
-set PFB_SOURCE_DIR=D:\php-firebird\
-
-for /f %%i in ('git -C %PFB_SOURCE_DIR%\php-firebird\ rev-parse --short HEAD') do set GIT_HASH=%%i
-
-@REM sets php-firebird version part in extension file, for example, php_interbase-<<3.0.1-ba8e63b>>-7.3-vc15.dll
-set PFB_VERS=3.0.1-%GIT_HASH%
+@REM Should point one level up. For example
+@REM PFB_SOURCE_DIR=D:\php-firebird\ then your source should reside in D:\php-firebird\php-firebird\
+set PFB_SOURCE_DIR=%~dp0..\..\
 
 @REM Directory where all compiled files will be copied
-set PFB_OUTPUT_DIR=D:\php-firebird\releases\
+set PFB_OUTPUT_DIR=%PFB_SOURCE_DIR%releases\
 
 @REM FB 32-bit and 64-bit libraries
 set PFB_FB32_DIR=C:\Program Files\Firebird\Firebird_5_0-x86
 set PFB_FB64_DIR=C:\Program Files\Firebird\Firebird_5_0
+
+@REM Attach current git commit hash. git command must be in PATH
+set USE_GIT_HASH=0
+
+for /f "tokens=3" %%i in ('findstr /b /c:"#define PHP_INTERBASE_VER_MAJOR" %~dp0..\php_interbase.h') do set VER_MAJOR=%%i
+for /f "tokens=3" %%i in ('findstr /b /c:"#define PHP_INTERBASE_VER_MINOR" %~dp0..\php_interbase.h') do set VER_MINOR=%%i
+for /f "tokens=3" %%i in ('findstr /b /c:"#define PHP_INTERBASE_VER_REV" %~dp0..\php_interbase.h') do set VER_REV=%%i
+set PFB_VERS=%VER_MAJOR%.%VER_MINOR%.%VER_REV%-beta
+
+if "%USE_GIT_HASH%" gtr "0" (
+	for /f %%i in ('git -C %~dp0..\ rev-parse --short HEAD') do set PFB_VERS=%PFB_VERS%-%%i
+)
